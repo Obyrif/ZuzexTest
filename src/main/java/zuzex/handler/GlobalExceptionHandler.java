ffe9c7a0f2e1c9ca5bdda09d8ac17e0a044d42eb
Obydennov Dmitrii <dima-obydennov@mail.ru>
2024-03-12 19:55:27 +0300

    finish

--- src/main/java/zuzex/handler/GlobalExceptionHandler.java
+++ src/main/java/zuzex/handler/GlobalExceptionHandler.java
@@ -2,10 +2,12 @@ package zuzex.handler;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import zuzex.exception.ResourceNotFoundException;
 
+@ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
